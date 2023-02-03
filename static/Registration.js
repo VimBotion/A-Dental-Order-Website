3e3cdefe61bd133bc721121f745c20318430ad23
Juan <juanpapolo123@gmail.com>
2023-02-03 17:54:14 -0500

    fix

--- static/Registration.js
+++ static/Registration.js
@@ -66,3 +66,4 @@ function setSuccessFor(input, message) {
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
